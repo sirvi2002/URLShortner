af1d633b2a9de74e68aaba624d434f53fe13d5e4
the-yash-rajput <yashrajputishu.gmail.com>
2022-10-30 20:24:37 +0530

    fixing directories

--- Backend/modules/shortURL.js
+++ Backend/modules/shortURL.js
@@ -28,36 +28,4 @@ router.post('/', async (req, res) => {
     // }
     console.log(req.body);
 });
-
-// router.get("/", async (req, res) => {
-//     try {
-//       await questionModel
-//         .aggregate([
-//           {
-//             $lookup: {
-//               from: "answers", //collection to join
-//               localField: "_id", //field from input document
-//               foreignField: "questionId",
-//               as: "allAnswers", //output array field
-//             },
-//           },
-//         ])
-//         .exec()
-//         .then((doc) => {
-//           res.status(200).send(doc);
-//         })
-//         .catch((error) => {
-//           res.status(500).send({
-//             status: false,
-//             message: "Unable to get the question details",
-//           });
-//         });
-//     } catch (e) {
-//       res.status(500).send({
-//         status: false,
-//         message: "Unexpected error",
-//       });
-//     }
-//   });
-
 module.exports = router;
